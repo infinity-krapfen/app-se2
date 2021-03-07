ce2bc627594da439a8cef3c892978509846f57d7
Nik <nitschernitz@edu.at>
2021-03-07 17:20:02 +0100

    Start

---
+++ app/src/main/java/com/example/app_se2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.app_se2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
